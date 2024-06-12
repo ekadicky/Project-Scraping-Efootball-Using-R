d080afa50e0c2662465e9c8109e497fdb2706894
Eka Dicky Darmawan Yanuari <142238683+ekadicky@users.noreply.github.com>
2024-06-12 19:05:44 +0700

    Update ProjectScraping.R

--- ProjectScraping.R
+++ ProjectScraping.R
@@ -1,21 +1,28 @@
+# Memuat library yang akan digunakan
 library(rvest)
 library(tidyverse)
 library(mongolite)
 
+# Masukkan link atau alamat website yang akan discraping
 alamatweb <- 'https://pesdb.net/efootball/?mode=authentic&featured=0'
 lamanweb <- read_html(alamatweb)
 
+# Mengambil tabel/data yang diinginkan
 tabel <- lamanweb %>% html_nodes(".players") %>% html_table()
+
+# Membentuk dataframe
 tabeldf <- data.frame(tabel)
 
+# Memilih satu baris data secara acak
 pilih <- sample(1:32,1,replace=F)
-player <- tabeldf[pilih,]
+player_terpilih <- tabeldf[pilih,]
 
+# Koneksi ke MongoDB untuk memasukkan data
 atlas_conn <- mongo(
   collection = Sys.getenv("ATLAS_COLLECTION"),
   db         = Sys.getenv("ATLAS_DB"),
   url        = Sys.getenv("ATLAS_URL")
 )
 
-atlas_conn$insert(player)
+atlas_conn$insert(player_terpilih)
 rm(atlas_conn)
